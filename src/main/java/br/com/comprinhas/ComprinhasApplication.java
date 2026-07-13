3940c04306fb50f9b9c3c9f6a5017ac6a4a2a7ad
Yasmin <yasminarnaut87@gmail.com>
2026-07-13 14:45:55 -0300

    criação repositorio

---
+++ src/main/java/br/com/comprinhas/ComprinhasApplication.java
@@ -0,0 +1,13 @@
+package br.com.comprinhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprinhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComprinhasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/comprinhas/ComprinhasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.comprinhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprinhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
